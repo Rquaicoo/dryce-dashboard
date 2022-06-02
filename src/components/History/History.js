bd5e6f80d4d6d2eab098a7ce030be944f3fe7218
Reu-Amissah <reujoyamissah@gmail.com>
2022-06-02 11:30:30 +0000

    add history table

--- src/components/History/History.js
+++ src/components/History/History.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './History.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 const History = () => {
@@ -10,36 +11,51 @@ const History = () => {
         <h1 className='history-header'>History</h1>
         <table id="history-table">
           <tr id="table-header">
-            <th>ID</th>
+            <th> <FontAwesomeIcon icon="fa-solid fa-user" />ID</th>
             <th>Client Info</th>
             <th>Status</th>
             <th>Order Date</th>
             <th>Delivery Date</th>
           </tr>
           <tr>
-            <td>Alfreds Futterkiste</td>
-            <td>Maria Anders</td>
-            <td>Germany</td>
+            <td>5543</td>
+            <td className='cus-name'>Maria Anders<br/><span className='name-description'>Accra</span></td>
+            <td><span className='status'>In Progress</span></td>
+            <td>3/8/22</td>
+            <td>5/8/22</td>
           </tr>
           <tr>
-            <td>Berglunds snabbköp</td>
-            <td>Christina Berglund</td>
-            <td>Sweden</td>
+            <td>5543</td>
+            <td className='cus-name'>Prince Charlse<br/><span className='name-description'>Takoradi</span></td>
+            <td><span className='status'>Oh Hold</span></td>
+            <td>3/8/22</td>
+            <td>5/8/22</td>
           </tr>
           <tr>
-            <td>Centro comercial Moctezuma</td>
-            <td>Francisco Chang</td>
-            <td>Mexico</td>
+            <td>5543</td>
+            <td className='cus-name'>Ekow Amissah<br/><span className='name-description'>Madina</span></td>
+            <td><span className='status'>Delivered</span></td>
+            <td>3/8/22</td>
+            <td>5/8/22</td>
           </tr>
           <tr>
-            <td>Ernst Handel</td>
-            <td>Roland Mendel</td>
-            <td>Austria</td>
+            <td>5543</td>
+            <td className='cus-name'>Alfred Yeboah<br/><span className='name-description'>Cape Coast</span></td>
+            <td><span className='status'>Cancelled</span></td>
+            <td>3/8/22</td>
+            <td>5/8/22</td>
           </tr>
           <tr>
-            <td>Island Trading</td>
-            <td>Helen Bennett</td>
-            <td>UK</td>
+            <td>5543</td>
+            
+            <td className='cus-name'>Maria Anders
+              <br/>
+              <span className='name-description'>Accra</span>
+            </td>
+
+            <td><span className='status'>In Progress</span></td>
+            <td>3/8/22</td>
+            <td>5/8/22</td>
           </tr>
         </table>
 
